768bd7a5b68b690e29cc10dd4f6c2376213aca3f
ganesh <192110248.sse.@gmail.com>
2026-03-01 16:25:09 +0530

    Added Max_in_Array problem

---
+++ Arrays/Max_in_Array.java
@@ -0,0 +1,11 @@
+public class Max_in_Array {
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
