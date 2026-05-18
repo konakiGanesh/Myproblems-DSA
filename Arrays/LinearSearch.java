a735a36e8aeec21619369a7f5c2982c3fac03f35
ganesh <192110248.sse.@gmail.com>
2026-05-18 19:43:09 +0530

    Save local changes to LinearSearch

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -5,8 +5,9 @@ public class LinearSearch {
         int target = 45;
         for(int i=0; i<n ; i++){
             if(arr[i]==target){
+                found=true;
                 System.out.println("Element found at index: " + i); //index =i this returns the indexof the target element
-                return; // this will exit the main method after finding the target element
+                break;
             }
         }
         System.out.println("Element not found in the array"); // this will print if the target element is not found in the array
